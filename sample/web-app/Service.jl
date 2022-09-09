36ab39da0c8697838ad9f56273161ce30ff0886d
Masakazu Kizawa <30505094+mskz930@users.noreply.github.com>
2022-09-09 12:57:03 +0900

    Create Service.jl

---
+++ sample/web-app/Service.jl
@@ -0,0 +1,21 @@
+module Service.jl
+
+module TodoService.jl
+
+function createTodo(dto::TodoDto)
+end
+
+function deleteTodo(dto::TodoDto)
+end
+
+
+end # module
+
+
+module LoginService
+
+
+end # module
+
+
+end # module
